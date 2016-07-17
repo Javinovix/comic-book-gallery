f0b26083f7803b6adcc86d38cc2a30d5a83d072a
Javinovix <javinovix@gmail.com>
2016-07-17 11:37:53 -0400

    Added comic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,14 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
+
+            return View();
+            /*if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
             {
                 return Redirect("/");
             }
             return Content("Hello from the comic book store");
-         
+         */
         }
     }
 }
